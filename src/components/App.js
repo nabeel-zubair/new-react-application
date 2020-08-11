86c91163816ed6323e2767eb4c5993b4869f9720
Nabeel Zubair <nabeel@devigital.com>
2020-08-11 15:25:38 +0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>My React Application</h1>
+            </div>
+        )
+    }
+}
+
+export default App
