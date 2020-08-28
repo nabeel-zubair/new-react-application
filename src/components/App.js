5717a53052a7c00abcd14cfe2e6c59a1dc6d0307
Nabeel Zubair <nabeel@devigital.com>
2020-08-28 15:19:29 +0500

    Removed material components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import '../styles/App';
-import NavBar from './NavBar';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <NavBar />
+                <h1>My React Application</h1>
             </div>
         )
     }
--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -4,12 +4,3 @@ export const appStrings = {
     appTitle: 'My React Application',
     buttonText: 'Login'
 }
-
-export const drawerItems = [
-    { id: 1, name: 'List Item 1' },
-    { id: 2, name: 'List Item 2' },
-    { id: 3, name: 'List Item 3' },
-    { id: 4, name: 'List Item 4' },
-    { id: 5, name: 'List Item 5' },
-    { id: 6, name: 'List Item 6' },
-]
