bf8b624b10b1cfd807275ff2c8c44071311c3ef9
Nabeel Zubair <nabeel@devigital.com>
2020-08-30 00:52:53 +0500

    Basic redux implementation

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'fontsource-roboto';
 
+import store from './store';
+import { bugAdded, bugResolved } from './actions/actionCreator';
+
+store.dispatch(bugAdded("Bug1"));
+store.dispatch(bugResolved(1));
+
+console.log(store.getState());
+
 ReactDOM.render(<App />, document.getElementById('root'));
